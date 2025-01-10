7789df1b1b3ba4715f865622a88254882ff769a7
hitko <hitko21@gmail.com>
2025-01-10 16:06:41 +0100

    in progress...

---
+++ vector_space/main.py
@@ -0,0 +1,14 @@
+class Vector:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def norm(self):
+        return (self.x ** 2 + self.y ** 2) ** 0.5
+
+    def __str__(self):
+        return
+
+
+v1 = Vector(2, 3)
+print(v1.norm())
