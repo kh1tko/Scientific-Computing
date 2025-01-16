b87e7a847a945ef9fbb646898e12f977bdac0aee
hitko <hitko21@gmail.com>
2025-01-16 17:39:31 +0100

    in progress...

--- vector_space/main.py
+++ vector_space/main.py
@@ -1,14 +1,55 @@
-class Vector:
-    def __init__(self, x, y):
+class R2Vector:
+    def __init__(self, *, x, y):
         self.x = x
         self.y = y
 
     def norm(self):
-        return (self.x ** 2 + self.y ** 2) ** 0.5
+        return sum(val ** 2 for val in vars(self).values()) ** 0.5
 
     def __str__(self):
-        return
+        return str(tuple(getattr(self, i) for i in vars(self)))
+
+    def __repr__(self):
+        arg_list = [f'{key}={val}' for key, val in vars(self).items()]
+        args = ', '.join(arg_list)
+        return f'{self.__class__.__name__}({args})'
+
+    def __add__(self, other):
+        if type(self) != type(other):
+            return NotImplemented
+        kwargs = {i: getattr(self, i) + getattr(other, i) for i in vars(self)}
+        return self.__class__(**kwargs)
+
+    def __sub__(self, other):
+        if type(self) != type(other):
+            return NotImplemented
+        kwargs = {i: getattr(self, i) - getattr(other, i) for i in vars(self)}
+        return self.__class__(**kwargs)
+
+    def __mul__(self, other):
+        if isinstance(other, (float, int)):
+            kwargs = {i: getattr(self, i) * other for i in vars(self)}
+            return self.__class__(**kwargs)
+        elif type(self) == type(other):
+            kwargs = {i: getattr(self, i) * getattr(other, i) for i in vars(self)}
+            return self.__class__(**kwargs)
+        else:
+            return NotImplemented
 
 
-v1 = Vector(2, 3)
-print(v1.norm())
+class R3Vector(R2Vector):
+    def __init__(self, *, x, y, z):
+        super().__init__(x=x, y=y)
+        self.z = z
+
+
+v1 = R2Vector(x=2, y=3)
+v2 = R2Vector(x=0.5, y=1.25)
+print(f'v1 = {v1}')
+print(f'v2 = {v2}')
+v3 = v1 + v2
+print(f'v1 + v2 = {v3}')
+v4 = v1 - v2
+print(f'v1 - v2 = {v4}')
+v5 = v1 * v2
+print(f'v1 * v2 = {v5}')
